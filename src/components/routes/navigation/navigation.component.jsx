0eadc34201ee3052fe7578de5b86d94469693490
Jannis G <trairotte@gmail.com>
2023-06-08 17:26:08 +0200

    basic navbar styling

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,27 +1,35 @@
 import { Link, Outlet } from 'react-router-dom';
 import './navigation.styles.scss';
+import { ReactComponent as LovedIcon } from '../../../assets/vector-graphics/heart-icon.svg';
+import { ReactComponent as LoginIcon } from '../../../assets/vector-graphics/login-icon.svg';
+import { ReactComponent as ShoppingCartIcon } from '../../../assets/vector-graphics/shopping-cart-icon.svg';
+import { ReactComponent as PathosLogo } from '../../../assets/vector-graphics/pathos-logo.svg';
 
 const Navigation = () => {
     return(
         <div className="navigation-container">
-            <div className="navigation-item-wrapper">
-                <div className="navigation-logo">
-                    <Link  className="nav-link" to="/">
-                        Logo
-                    </Link>
+            <div className="navigation-items-wrapper">
+                <div>
+                    <Link to="/store" className="nav-category-link">Store</Link>
+                    <Link to="/about" className="nav-category-link">About</Link>
                 </div>
 
                 <div className="link-container-categories">
-                    <Link className="nav-link">Mens</Link>
-                    <Link className="nav-link">Womens</Link>
-                    <Link className="nav-link">Mens</Link>
-                    <Link className="nav-link">Misc</Link>
+                    <Link className="nav-link" to="/">
+                        <PathosLogo className="pathos-logo"/>
+                    </Link>
                 </div>
 
                 <div className="utility-icon-container">
-                    <Link  className="nav-link">Loved</Link>
-                    <Link  className="nav-link">Cart</Link>
-                    <Link  className="nav-link" to="auth">Sign In</Link>
+                    <Link  className="nav-utility-icon-wrapper">
+                        <LovedIcon  className="nav-utility-icon"/>
+                    </Link>
+                    <Link  className="nav-utility-icon-wrapper">
+                        <ShoppingCartIcon  className="nav-utility-icon"/>
+                    </Link>
+                    <Link  className="nav-utility-icon-wrapper" to="auth">
+                        <LoginIcon  className="nav-utility-icon"/>
+                    </Link>
                 </div>
             </div>
             <Outlet />
