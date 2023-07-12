dbcd40a0d7c5d70233475c29ca8ffaad0ac7c712
Jannis G <trairotte@gmail.com>
2023-07-12 17:24:23 +0200

    some cosmetic improvements

--- src/components/routes/checkout/checkout.component.jsx
+++ src/components/routes/checkout/checkout.component.jsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+import { CartContext } from '../../../context/cart-context';
 import './checkout.styles.scss';
 
+
 const CheckoutPage = () => {
+
+    const {} = useContext(CartContext);
+    
     return (
         <div>
             <h1>oidpiwejdpiwejpdjewidjiewjdwjdweoidjwojdowe chekoout</h1>
